ff97819463166ebdcac7052ee58f3eefa4762e3a
Marina Massidda <marinamassidda@dhcp-10-31-245-157.wireless.dartmouth.edu>
2015-02-28 22:46:35 -0500

    Initial Commit from Local Git

---
+++ AppOne/AppOne.h
@@ -0,0 +1,19 @@
+//
+//  AppOne.h
+//  AppOne
+//
+//  Created by Marina Massidda on 2/28/15.
+//  Copyright (c) 2015 LotaEzenwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppOne.
+FOUNDATION_EXPORT double AppOneVersionNumber;
+
+//! Project version string for AppOne.
+FOUNDATION_EXPORT const unsigned char AppOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppOne/PublicHeader.h>
+
+
